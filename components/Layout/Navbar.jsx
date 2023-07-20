332b5af26bb972e583cbc9a4232199d2fde087ed
maork-elementor <maork@elementor.com>
2023-07-20 16:43:03 +0300

    update

--- components/Layout/Navbar.jsx
+++ components/Layout/Navbar.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import Link from 'next/link'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+  const handleMobileMenuToggle = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen)
+  }
+
   return (
     <header>
-      <nav className='bg-white border-gray-200 dark:bg-gray-900'>
+      <nav className='bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700'>
         <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-          <a href='https://flowbite.com/' className='flex items-center'>
+          <a href='#' className='flex items-center'>
             <img
               src='https://flowbite.com/docs/images/logo.svg'
               className='h-8 mr-3'
@@ -17,10 +22,11 @@ const Navbar = () => {
             </span>
           </a>
           <button
-            data-collapse-toggle='navbar-default'
+            onClick={handleMobileMenuToggle}
+            data-collapse-toggle='navbar-multi-level'
             type='button'
             className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
-            aria-controls='navbar-default'
+            aria-controls='navbar-multi-level'
             aria-expanded='false'
           >
             <span className='sr-only'>Open main menu</span>
@@ -40,39 +46,45 @@ const Navbar = () => {
               />
             </svg>
           </button>
-          <div className='hidden w-full md:block md:w-auto' id='navbar-default'>
-            <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+          <div
+            className={`${
+              isMobileMenuOpen ? 'block' : 'hidden'
+            } w-full md:block md:w-auto`}
+            id='navbar-multi-level'
+          >
+            <ul className='flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
               <li>
-                <Link 
-                href='/'
-                className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
+                <a
+                  href='#'
+                  className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent'
+                  aria-current='page'
                 >
                   Home
-                </Link>
+                </a>
               </li>
               <li>
-              <Link 
-                href='/search'
-                className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
+                <a
+                  href='#'
+                  className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
                 >
-                  Search
-                </Link>
+                  Services
+                </a>
               </li>
               <li>
-              <Link
-                href='/contact'
-                className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
+                <a
+                  href='#'
+                  className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+                >
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a
+                  href='#'
+                  className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
                 >
                   Contact
-                </Link>
-              </li>
-              <li>
-              <Link
-                href='/about'
-                className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
-                >
-                  About
-                </Link>
+                </a>
               </li>
             </ul>
           </div>
--- components/UI/Button.js
+++ components/UI/Button.js
@@ -1,6 +1,5 @@
 // nextjs button
 import React from 'react';
-import Link from 'next/link';
 
 const Button = () => {
     return (
