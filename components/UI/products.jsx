cd66b4c71670ff48d5c3eb59abe4b6ecc0fb8859
maork-elementor <maork@elementor.com>
2023-07-21 21:52:09 +0300

    update

---
+++ components/UI/products.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react'
+
+const Products = () => {
+  const [data, setData] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [offset, setOffset] = useState(0)
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=10&skip=${offset}&select=title,price`
+      )
+      const newData = await response.json()
+      // debugger
+      setData(prevData => [...prevData, ...newData.products])
+      setIsLoading(false)
+    } catch (error) {
+      console.error('Error fetching data:', error)
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [offset])
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      setOffset(prevoffset => prevoffset + 10)
+    }
+  }
+
+  const loader = () => {
+    return (
+      <div className='border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto'>
+        <div className='animate-pulse flex space-x-4'>
+          <div className='rounded-full bg-slate-200 h-10 w-10'></div>
+          <div className='flex-1 space-y-6 py-1'>
+            <div className='h-2 bg-slate-200 rounded'></div>
+            <div className='space-y-3'>
+              <div className='grid grid-cols-3 gap-4'>
+                <div className='h-2 bg-slate-200 rounded col-span-2'></div>
+                <div className='h-2 bg-slate-200 rounded col-span-1'></div>
+              </div>
+              <div className='h-2 bg-slate-200 rounded'></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  return (
+    <>
+    <div>
+      {data.map(item => (
+        <div key={item.id} style={{
+          height: '100px',
+        }}>
+          <h1>{item.title}</h1>
+        </div>
+      ))}
+      </div>
+      {isLoading && loader()}
+    </>
+  )
+}
+
+export default Products
--- pages/home.jsx
+++ pages/home.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { getData }  from '../utils/data'
 import PropTypes from 'prop-types';
-
+import Products from '../components/UI/products'
 
 export default function Home ({ data }) {
   return (
     <div className='flex max-w-screen-xl mx-auto'>
       <div className='flex-1 w-30'>
-      {data.map((item) => (
+      {/* {data.map((item) => (
         <div key={item.id}>
           <h1>{item.title}</h1>
           </div>
-      ))}
+      ))} */}
       </div>
       <div className='flex-1 w-70'>
-
+        <Products />
       </div>
       <div className='flex-1 w-30'>
         
